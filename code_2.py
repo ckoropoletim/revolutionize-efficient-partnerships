e9140be454e66519fe0aa7bc04c63bf1cd109ab8
ckoropoletim <143329990+ckoropoletim@users.noreply.github.com>
2024-01-31 04:02:48 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,14 @@
+def main():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+import random
+    data = generate_random_data()
+    main()
+
+def generate_random_data():
+    return data
+        print(f"Random Number: {item}")
+    for item in data:
+if __name__ == "__main__":
